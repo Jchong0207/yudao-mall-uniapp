12594128622c1ad7c143549a14fb09572eedf598
Jchong0207 <167512151+Jchong0207@users.noreply.github.com>
2025-01-31 15:27:20 +0800

    Delete dist/build/web directory

--- dist/build/web/assets/point.DvvdllU8.js
+++
@@ -1,1 +0,0 @@
-import{aa as t}from"./index-B9abLD3F.js";const i={getPointActivityPage:i=>t({url:"promotion/point-activity/page",method:"GET",params:i}),getPointActivityListByIds:i=>t({url:"/promotion/point-activity/list-by-ids",method:"GET",params:{ids:i}}),getPointActivity:i=>t({url:"promotion/point-activity/get-detail",method:"GET",params:{id:i}})};export{i as P};
--- dist/build/web/assets/rewardActivity.1DzahVsD.js
+++
@@ -1,1 +0,0 @@
-import{aa as t}from"./index-B9abLD3F.js";const a={getRewardActivity:a=>t({url:"/promotion/reward-activity/get",method:"GET",params:{id:a}})};export{a as R};
