db63c28aa2def1c4b8e61e4fc8f8572afbd1c00c
JC <ngjingchong@gmail.com>
2025-02-06 01:24:24 +0800

    remove dist

--- dist/build/web/assets/config.ZxeeTMW2.js
+++
@@ -1,1 +0,0 @@
-import{aa as o}from"./index-Dg3xN8dW.js";const t={getTradeConfig:()=>o({url:"/trade/config/get",method:"GET",custom:{showLoading:!1}})};export{t as T};
--- dist/build/web/assets/rewardActivity.BWYvYjZi.js
+++
@@ -1,1 +0,0 @@
-import{aa as t}from"./index-Dg3xN8dW.js";const a={getRewardActivity:a=>t({url:"/promotion/reward-activity/get",method:"GET",params:{id:a}})};export{a as R};
