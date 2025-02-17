803933c90ef7bd1ab1d606fcc88f2cbbea728172
JC <ngjingchong@gmail.com>
2025-02-17 19:56:11 +0800

    update dist file

---
+++ dist/build/web/assets/article.DOo-ooRZ.js
@@ -0,0 +1,1 @@
+import{aa as t}from"./index-Bw34y8dl.js";const e={getArticle:(e,o)=>t({url:"/promotion/article/get",method:"GET",params:{id:e,title:o}})};export{e as A};
---
+++ dist/build/web/assets/config.S_fJaQn5.js
@@ -0,0 +1,1 @@
+import{aa as o}from"./index-Bw34y8dl.js";const t={getTradeConfig:()=>o({url:"/trade/config/get",method:"GET",custom:{showLoading:!1}})};export{t as T};
